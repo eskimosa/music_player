d9a99d7162bff66859e45db7ac6c7bc0a1e548a4
eskimosa <esskimosa@gmail.com>
2024-11-13 18:33:11 +0100

    worked on progress bar - updating width of the progress

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -87,6 +89,19 @@ function nextSong() {
 // On Load - Select First Song
 loadSong(songs[3]);
 
+// Update Progress Bar and time
+
+function updateProgressBar(e) {
+    if (isPlaying) {
+        const { duration, currentTime } = e.srcElement;
+        // Update the progress bar width
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+        
+    }
+}
+
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
